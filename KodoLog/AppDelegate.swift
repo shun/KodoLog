dbdce2e66572b5f9853043dab0332b06735cc081
Shunsuke Kudo <skudo_xx@yahoo.co.jp>
2015-07-20 23:29:57 +0900

    add icon

--- KodoLog/AppDelegate.swift
+++ KodoLog/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         UIDevice.currentDevice().batteryMonitoringEnabled = true
+        sleep(2)
         m_maincontroller = AppMainController()
         return true
     }
