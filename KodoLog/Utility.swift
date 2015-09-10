be4212167860ac3af9486c87ca24bfd606c91c3f
Shunsuke Kudo <skudo_xx@yahoo.co.jp>
2015-09-10 23:42:50 +0900

    add convert utility with RGB code

--- KodoLog/Utility.swift
+++ KodoLog/Utility.swift
@@ -15,4 +15,22 @@ class Utility: NSObject {
         dateFormatter.dateFormat = format                           // フォーマットの指定
         return dateFormatter.stringFromDate(date)
     }
+
+    class func getColorByRGB(rgba:UInt) ->UIColor {
+        return UIColor(
+            red:    CGFloat((rgba & 0xFF000000) >> 24) / 255.0,
+            green:  CGFloat((rgba & 0x00FF0000) >> 16) / 255.0,
+            blue:   CGFloat((rgba & 0x0000FF00) >> 8) / 255.0,
+            alpha:  CGFloat(rgba & 0x000000ff) / 255.0
+        )
+    }
+
+    class func getColorByRGB(r:UInt, g:UInt, b:UInt) ->UIColor {
+        return getColorByRGB(r, g: g, b: b, a: 255)
+    }
+
+    class func getColorByRGB(r:UInt, g:UInt, b:UInt, a:UInt) ->UIColor {
+        let rgba = (r << 24) | (g << 16) | (b << 8) | a
+        return getColorByRGB(rgba)
+    }
 }
